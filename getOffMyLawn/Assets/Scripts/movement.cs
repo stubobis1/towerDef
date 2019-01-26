e849be6d586399c6ecc4e4237617e03db44242ea
John <andmag77@gmail.com>
2019-01-26 02:29:09 -0700

    Movement
    Movement created, not including  mouse look

---
+++ getOffMyLawn/Assets/Scripts/movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Camera m_Camera;
+    private static float speed = 0.1f;
+    public float maxBoutns = 4.4f;
+    public float minBounds = -4.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        float xinput = Input.GetAxis("Horizontal");
+        this.transform.position = new Vector3(Mathf.Clamp((this.transform.position.x + (xinput * speed)),minBounds,maxBoutns), this.transform.position.y, this.transform.position.z);
+
+
+
+    }
+}
