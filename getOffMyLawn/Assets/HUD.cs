6f4f541e593b7be7849969946a4c6af5b4d10b3c
Stuart Greenlief <stuartgreenlief@workfront.com>
2019-01-26 13:46:09 -0700

    Merge branch 'UI-moneyBar'
    # Conflicts:
    #	getOffMyLawn/Assets/Material.meta
    #	getOffMyLawn/Assets/Plugins.meta
    #	getOffMyLawn/Assets/Plugins/Editor.meta
    #	getOffMyLawn/Assets/Plugins/Editor/JetBrains.meta
    #	getOffMyLawn/Assets/Plugins/Editor/JetBrains/JetBrains.Rider.Unity.Editor.Plugin.Repacked.dll.meta
    #	getOffMyLawn/Assets/Scripts/utility.meta

---
+++ getOffMyLawn/Assets/HUD.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public static HUD Instance;
+
+    public Text LivesText;
+    public UnityEngine.UI.Image MoneyBar;
+    public int Lives
+    {
+        get { return _lives; }
+        set
+        {
+            this.LivesText.text = "Lives: " + _lives;
+            _lives = value;
+        }
+    }
+
+    public int _lives;
+
+    public float _precentageOfMoney;
+    public float PrecentageOfMoney
+    {
+        get { return _precentageOfMoney; }
+        set
+        {
+            
+            var theBarRectTransform = MoneyBar.transform as RectTransform;
+            var precentage = value / 100f;
+            //THIS IS HARDCODED
+            theBarRectTransform.sizeDelta = new Vector2 ((Screen.width * 4) * precentage  , theBarRectTransform.sizeDelta.y);
+/*
+            Rect r = new Rect(this.MoneyBar.rectTransform.rect);
+            r.width = value / Screen.width;
+            var u = new RectTransform();
+            
+            this.MoneyBar.rectTransform.rect = r;  
+            _precentageOfMoney = value;*/
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        this.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PrecentageOfMoney = 50f;
+        Lives = _lives;
+    }
+}
