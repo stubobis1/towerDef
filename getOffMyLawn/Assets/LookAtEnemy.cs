b624c05eeb27b44e1fd75f270153aca6761687c6
Stuart Greenlief <stuartgreenlief@workfront.com>
2019-01-26 12:04:47 -0700

    cameraLerping is working now

--- getOffMyLawn/Assets/LookAtEnemy.cs
+++ getOffMyLawn/Assets/LookAtEnemy.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿  using System;
+  using System.Collections;
 using System.Collections.Generic;
-using DefaultNamespace;
+  using System.Linq.Expressions;
+  using DefaultNamespace;
 using UnityEngine;
 
 public class LookAtEnemy : MonoBehaviour
@@ -12,14 +14,34 @@ public class LookAtEnemy : MonoBehaviour
     }
 
     public GameObject target;
+
+    public float lookSpeed = 0.5f;
     // Update is called once per frame
     void Update()
     {
         if (target == null)
         {
-            target = AIManager.Instance.GetComponentInChildren<AIMovement>().gameObject ;
+            var ai = AIManager.Instance.GetComponentInChildren<AIMovement>();
+            target = ai != null ? ai.gameObject : null;
         }
+        else
+        {   
+            SmoothLook(target.transform.position - transform.position);
+        }
+        
+        /*
 
-        this.transform.LookAt(target.transform);
+        Vector3 direction = target.transform.position - transform.position;
+        Quaternion toRotation = Quaternion.FromToRotation(transform.up, direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, lookSpeed * Time.deltaTime);
+        */
+
+        /*Vector3 direction = target.transform.position - transform.position;
+        Quaternion toRotation = Quaternion.FromToRotation(transform.forward, direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, lookSpeed * Time.time);*/
+    }
+    void SmoothLook(Vector3 newDirection){
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newDirection), Time.deltaTime * lookSpeed);
     }
 }
+
