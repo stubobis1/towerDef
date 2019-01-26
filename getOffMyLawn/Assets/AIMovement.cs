6fc97faa19e05fb3cd22cfd88b399a496f0b6a37
Stuart Greenlief <stuartgreenlief@workfront.com>
2019-01-26 00:45:34 -0700

    WAYPOINTZZZ

---
+++ getOffMyLawn/Assets/AIMovement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+    private int targetIndex = 0;
+
+
+    public float Speed = 1f;
+    public float Acceleration = 1f;
+    // Start is called before the first frame update
+    public WaypointHandler Waypoints;
+    public Waypoint[] Targets;
+    private Rigidbody body;
+    void Start()
+    {
+        print("Start AI");
+        Targets = Waypoints.Waypoints;
+        body = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HitTrigger(Waypoint other)
+    {
+        //if (other == Targets[targetIndex]) {
+            if (targetIndex + 1 >= Targets.Length)
+            {
+                print("THISNHISXNINFS");
+                GameManager.Instance.EnemyGotThrough(this.gameObject);
+                Destroy(this.gameObject);
+            }
+
+            else
+            {
+                targetIndex++;
+            }
+
+        //}
+    }
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(Targets[targetIndex].transform);
+        body.AddForce(transform.forward * Speed,ForceMode.VelocityChange);
+        body.velocity = Vector3.Lerp(body.velocity,transform.forward * Speed, Acceleration); 
+    }
+}
---
+++ getOffMyLawn/Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    public void EnemyGotThrough(GameObject enemy)
+    {
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
