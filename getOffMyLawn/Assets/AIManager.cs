93c872faec74a7ac6ee058378fcb756de61fae32
Stuart Greenlief <mr.stubob@gmail.com>
2019-01-26 09:49:25 -0700

    AIManager - levels and stuff.

--- getOffMyLawn/Assets/AIManager.cs
+++ getOffMyLawn/Assets/AIManager.cs
@@ -3,30 +3,49 @@ using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
 
+
 public class AIManager : MonoBehaviour
 {
-    public int level;
+    public static AIManager Instance;
+
+    public bool activelySending = false;
     public WaypointHandler waypoints;
-    public GameObject[] EnemyPrefabs; //We might want something else to do enemies instead of an array (for bosses and such)
+    public GameObject[] EnemiesInWave;
+    public int EnemyIndex = 0;
     public float SendInterval = 3f;
 
     private float timeToSendNext;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > timeToSendNext)
+        if (activelySending)
         {
-            timeToSendNext = Time.time + SendInterval;
-            var pos = waypoints.Waypoints[0].transform.position;
-            var ai = Instantiate(EnemyPrefabs[0], this.transform);
-            ai.transform.position = pos;
-            ai.GetComponent<IHasWaypoints>().Waypoints = this.waypoints;
+            if (Time.time > timeToSendNext)
+            {
+                if (EnemyIndex + 1 <= EnemiesInWave.Length)
+                {
+                    timeToSendNext = Time.time + SendInterval;
+                    var pos = waypoints.Waypoints[0].transform.position;
+                    var ai = Instantiate(EnemiesInWave[EnemyIndex], this.transform);
+                    ai.transform.position = pos;
+                    ai.GetComponent<IHasWaypoints>().Waypoints = this.waypoints;
+                    EnemyIndex++;
+                }
+                else
+                {
+                    GameManager.Instance.FinishedLevel();
+                    
+                    //reset
+                    this.activelySending = false;
+                    EnemyIndex = 0;
+                }
+            }
         }
     }
 }
--- getOffMyLawn/Assets/Scripts/GameManager.cs
+++ getOffMyLawn/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,18 +7,97 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance; 
     // Start is called before the first frame update
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+    public GameObject Enemy3;
+    public GameObject Enemy4;
+    public GameObject Enemy5;
+
+    public int Level = -1;
+
+    public List<GameObject[]> Levels = new List<GameObject[]>(); //Each item in the array will be an array of enemies to send.
+
+    #region Levels
+
+    
+
+    List<GameObject[]> GetLevels()
+    {
+        var l = new List<GameObject[]>(); 
+        //Level 1
+        l.Add( new []{
+            Enemy1,
+            Enemy1,
+            Enemy1,
+            Enemy1,
+            Enemy1,
+        }); 
+        //Level 2
+        l.Add( new []{
+            Enemy1,
+            Enemy2,
+            Enemy2,
+            Enemy1,
+            Enemy2,
+            Enemy1,
+        }); 
+        //Level 3
+        l.Add( new []{
+            Enemy2,
+            Enemy2,
+            Enemy2,
+            Enemy2,
+            Enemy2,
+        });
+        return l;
+    }
+    
+    
+    #endregion
+
     void Start()
     {
         Instance = this;
+        this.Levels = GetLevels();
+    }
+
+    bool GameStarted = false;
+    public void StartGame()
+    {
+        GameStarted = true;
+        print("Starting Game!");
+        Level = 0;
+        AIManager.Instance.EnemiesInWave = Levels[0];
+    }
+    
+    
+    
+    void Update()
+    {
+        if(!GameStarted)
+            StartGame();
+
+        if (Input.GetButtonDown("Fire1") && !AIManager.Instance.activelySending)
+        {
+            //start if we haven't yet.
+            AIManager.Instance.activelySending = true;
+        }
     }
 
     public void EnemyGotThrough(GameObject enemy)
     {
     }
-    
-    // Update is called once per frame
-    void Update()
+
+    public void FinishedLevel()
     {
+        print("Finished Level: " + Level);
+        Level++;
+        AIManager.Instance.EnemiesInWave = Levels[Level];
+        
+        
         
     }
+    
+    // Update is called once per frame
+
 }
