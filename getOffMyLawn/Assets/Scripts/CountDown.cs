8257186a5941eb35d30ba20ac47768bbf5663a32
John <andmag77@gmail.com>
2019-01-27 15:21:21 -0700

    time and menu
    TIme and menu

---
+++ getOffMyLawn/Assets/Scripts/CountDown.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using UnityEngine.SceneManagement;
+public class CountDown : MonoBehaviour
+{
+    public int timeLeft = 120;
+    public int timeOneMin = 0;
+    public Text countdown;
+    void Start()
+        
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1; 
+    }
+    void Update()
+    {
+        if (timeLeft == 120)
+
+        {
+            countdown.text = ("2:00");
+        }
+
+       if(timeLeft >= 60 && timeLeft != 120)
+        {
+           timeOneMin = timeLeft - 60;
+            countdown.text = ("1:") + timeOneMin;
+      
+        }
+        if(timeLeft < 60)
+        {
+            countdown.text = ("0:" + timeLeft);
+        }
+        if(timeLeft <= 9)
+        {
+            countdown.text = ("0:0" + timeLeft);
+        }
+        if (timeLeft <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+
+        }
+    }
+    
+    
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
---
+++ getOffMyLawn/Assets/Scripts/EXIT.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EXIT : MonoBehaviour
+{
+    public void ExitGame()
+
+    {
+        Debug.Log("EXIT");
+        Application.Quit();
+    }
+}
