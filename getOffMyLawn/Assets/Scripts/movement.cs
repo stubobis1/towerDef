26bf49175014c283a766d43beec063deb445dc14
improvshark <improvshark@gmail.com>
2019-01-27 16:25:07 -0700

    skybox

--- getOffMyLawn/Assets/Scripts/movement.cs
+++ getOffMyLawn/Assets/Scripts/movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     private Camera m_Camera;
-    private static float speed = 0.1f;
+    public float speed = 0.1f;
     public float maxBoutns = 4.4f;
     public float minBounds = -4.5f;
 
@@ -18,10 +18,10 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
 
         float xinput = Input.GetAxis("Horizontal");
-        this.transform.position = new Vector3(Mathf.Clamp((this.transform.position.x + (xinput * speed)),minBounds,maxBoutns), this.transform.position.y, this.transform.position.z);
+        this.transform.position = new Vector3(Mathf.Clamp((this.transform.position.x + (xinput * speed * Time.deltaTime)),minBounds,maxBoutns), this.transform.position.y, this.transform.position.z);
 
 
 
